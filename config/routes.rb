34771923e4059a8a104a77492e150cdafcd44717
SupremeA <supreme036@gmail.com>
2016-01-31 18:09:57 -0600

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 MarketPlaceApi::Application.routes.draw do
-  
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+
+  end
+
 end
