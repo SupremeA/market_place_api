b5ef4055ed0f7dfe43207ad6c5e529d7e543367b
SupremeA <supreme036@gmail.com>
2016-02-20 16:11:22 -0600

    Allows the order to be placed and decreases the quantity of products available

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :product do
     title { FFaker::Product.product_name }
     price { rand() * 100 }
+    quantity 87
     published false
     description "This thing is great!"
     user
